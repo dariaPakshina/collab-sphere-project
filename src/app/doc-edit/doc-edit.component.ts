4eab5a84d9a77cbb95a494576111c1cdb543a9e2
Daria Pakshina <daradasa7@yandex.ru>
2024-11-08 21:40:26 +0300

    UI

--- src/app/doc-edit/doc-edit.component.ts
+++ src/app/doc-edit/doc-edit.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavDocEditComponent } from './nav-doc-edit/nav-doc-edit.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-doc-edit',
   standalone: true,
-  imports: [NavDocEditComponent],
+  imports: [
+    NavDocEditComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './doc-edit.component.html',
   styleUrl: './doc-edit.component.scss',
 })
-export class DocEditComponent {}
+export class DocEditComponent implements OnInit {
+  addForm!: FormGroup;
+  ngOnInit() {
+    this.addForm = new FormGroup({
+      title: new FormControl(null, [Validators.required]),
+      content: new FormControl(null),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.addForm.value);
+  }
+}
